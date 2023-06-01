2f483ee9e7bf77ce54554778f0f5ecaf5047af1b
Kevin Deakers <kmdeakers@mac.com>
2023-06-01 12:09:37 -0700

    done up to depthFirstSearch

---
+++ graph.js
@@ -0,0 +1,77 @@
+/** Node class for graph. */
+
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+
+/** Graph class. */
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  /** add Node instance and add it to nodes property on graph. */
+  addVertex(vertex) { 
+    this.nodes.add(vertex);
+  }
+
+  /** add array of new Node instances and adds to them to nodes property. */
+  addVertices(vertexArray) { 
+    for (const vertex of vertexArray) this.nodes.add(vertex);
+  }
+
+  /** add edge between vertices v1,v2 */
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1)
+   }
+
+  /** remove edge between vertices v1,v2 */
+  removeEdge(v1, v2) { 
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  /** remove vertex from graph:
+   *
+   * - remove it from nodes property of graph
+   * - update any adjacency lists using that vertex
+   */
+  removeVertex(vertex) { 
+    this.nodes.
+    delete(vertex);
+    for (const neighbor of vertex.adjacent) {
+      neighbor.adjacent.delete(vertex);
+    }
+  }
+
+  /** traverse graph with DFS and returns array of Node values */
+  depthFirstSearch(start) { 
+    const values = [];
+    function _depthFirstSearch (node, seen = new Set()) {
+      values.push(node.value);
+      seen.add(node);
+      for (const n of node.adjacent) {
+        if (!seen.has(n)) {
+          _depthFirstSearch(n, seen);
+        }
+      }
+    }
+    _depthFirstSearch(start);
+    console.log('values: ' ,values)
+    return values;
+  }
+
+  /** traverse graph with BDS and returns array of Node values */
+  breadthFirstSearch(start) { }
+
+  /** find the distance of the shortest path from the start vertex to the end vertex */
+  distanceOfShortestPath(start, end) { }
+}
+
+module.exports = { Graph, Node }
